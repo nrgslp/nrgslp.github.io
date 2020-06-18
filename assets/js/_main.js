6a0c2d549c0d19f5638d5dd53ee84ca6889bdbbe
nrgslp <53921196+nrgslp@users.noreply.github.com>
2020-06-17 21:12:47 -0400

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -3,6 +3,23 @@
    ========================================================================== */
 
 $(document).ready(function(){
+   // When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
    // Sticky footer
   var bumpIt = function() {
       $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
